77f814c86fb61c2bf611eba1b4c23ab9030da53e
Ketki <ketki.thosar@acellere.com>
2019-07-23 15:51:14 +0530

    removed code issue

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -50,12 +50,7 @@ if (process.env.NODE_ENV !== 'production') {
   typography.injectStyles();
 }
 
-//This is just a comment 
 
-
-if (process.env.NODE_ENV !== 'production') {
-  typography.injectStyles();
-}
 export default typography;
 export const rhythm = typography.rhythm;
 export const scale = typography.scale;
